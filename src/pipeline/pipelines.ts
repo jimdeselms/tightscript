48fbda03848a2d9a0389138a1592f1d9ca6eac13
Jim DeSelms <jdeselms@vista.com>
2025-03-28 14:59:18 -0400

    chain and link pipelines together

--- src/pipeline/pipelines.ts
+++ src/pipeline/pipelines.ts
@@ -20,17 +20,16 @@ export function linkPipelines<TIn, TOut, TSharedState, TMid>(
 export function chainPipelines<TIn, TOut, TState=unknown>(
     ...pipelines: Pipeline<unknown, unknown, TState>[]
 ): Pipeline<TIn, TOut, TState> {
-    return (onOut: (out: TOut) => void, initialState: TState) => {
+    return (onOut: (out: TOut) => void) => {
         if (pipelines.length === 0) {
             // If there are no input pipelines, that's the same as the identity function
             return { send: (input: TIn) => onOut(input as unknown as TOut) }
         }
-        
-        let currentPipeline: any = pipelines[0](onOut as any, initialState)
 
-        for (let i = 1; i < pipelines.length; i++) {
-            const nextPipeline = pipelines[i]
-            currentPipeline = linkPipelines(currentPipeline, nextPipeline)(onOut as any, initialState)
+        let currentPipeline: any = pipelines[pipelines.length-1](onOut as any)
+
+        for (let i = pipelines.length-2 ; i >= 0; i--) {
+            currentPipeline = pipelines[i](currentPipeline.send)
         }
 
         return currentPipeline
