06368e7cab25d2e354f875a1c9ea53228d7c7e13
Jim DeSelms <jdeselms@vista.com>
2025-05-06 14:13:40 -0400

    get rid of everything not related to interpreter

--- src/pipeline/evalPipeline.test.js
+++
@@ -1,8 +0,0 @@
-import { createEvalPipeline } from './evalPipeline'
-
-describe('evalPipeline', () => {
-    it('number', () => {
-        const pipeline = createEvalPipeline()
-        expect(pipeline(1)).toBe(1)
-    })
-})
--- src/pipeline/thing.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from 'vitest';
-import { hello } from './thing';
-
-describe('hello', () => {
-    it('should return HELLO!', () => {
-        expect(hello()).toBe('HELLO!');
-    });
-});
